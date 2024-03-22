f8dbadff3c3647a08191317471baedcb83b6297c
Darr Or <darrmorgan@gmail.com>
2024-03-22 11:21:12 -0400

    Add gallery-controller and two samples charts

--- js/gallery.controller.js
+++ js/gallery.controller.js
@@ -1,5 +1,14 @@
 'use strict'
 
+function onInit() {
+    renderGallery()
+}
+
 function renderGallery() {
-    
+
+    const strHtmls = `
+    <div class="sample1"><img src="img/bar-chart.png" alt=""></div>
+    <div class="sample2"><img src="img/circle-chart.jpg" alt=""></div>`
+
+    document.querySelector('.chart-samples').innerHTML = strHtmls
 }
